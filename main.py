f7d05494de56b15551ecb66eb243c53968ed734a
Aman Patel <amanpatel@Amans-MacBook-Pro.local>
2025-07-15 19:12:29 +0100

    pushing progress

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
-import os
 from dotenv import load_dotenv
-import requests
-import pandas as pd
 import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import pandas as pd
+import json
 
+# Load the .env file
 load_dotenv()
-client_id = os.getenv("client_id")
-client_secret = os.getenv("client_secret")
-spotify_redirect_uri = os.getenv("uri")
 
-url = "https://api.spotify.com/v1/me/player/recently-played?limit=50&after=1750940400000"
+# Authenticate with Spotify
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope="user-read-recently-played"))
 
-request = spotipy.
+# Fetch and print 1 recent track (as a test)
+results = sp.current_user_recently_played(limit=50, after=1751324400000)
+
+
+df = pd.json_normalize(results["items"])
+
+for col in df.columns:   # this prints all the columns in the df 
+    print(col)
+
+df = df[
+    ["track.album.artists", 
+    "track.album.name", 
+    "track.album.release_date", 
+    "track.artists", 
+    "track.duration_ms", 
+    "track.explicit",
+    "track.name",
+    "track.popularity"]
+    ]
+
+print(df)
